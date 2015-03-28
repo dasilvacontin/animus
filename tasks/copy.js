0595c906d61384f7b63853a53eacb60ee05786ba
yamadapc <tacla.yamada@gmail.com>
2015-03-28 12:35:28 -0300

    Add a basic manifest.json
    And add it to the copying tasks.

--- tasks/copy.js
+++ tasks/copy.js
@@ -2,6 +2,12 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 
+exports.other = function() {
+  return gulp.src('./app/**/*.json')
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload());
+};
+
 exports.html = function() {
   return gulp.src('./app/**/*.html')
     .pipe(gulp.dest('./dist'))
